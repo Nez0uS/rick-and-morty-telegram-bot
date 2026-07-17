8d3f6da91d13a3a78cc727e2513be5740b98e65a
Nez0uS <sergei.sam2110@gmail.com>
2026-07-17 14:29:30 +0400

    fix: correct start router name in dispatcher

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import Config
 
-from handlers.handler import router
+from handlers.handler import start_router
 from handlers.random_charachter import random_character_router as random_character
 from handlers.search_by_name import search_by_name_router as search_by_name
 from handlers.search_by_status import search_by_status_router as search_by_status
@@ -18,7 +18,7 @@ async def main():
     bot = Bot(config.TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
 
-    dp.include_router(router)
+    dp.include_router(start_router)
     dp.include_router(random_character)
     dp.include_router(search_by_name)
     dp.include_router(search_by_status)
